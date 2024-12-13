1797b1490d7fe3d3301476e9d6fe676a88e44caf
Sornnacha Buranapongwattana <sornnacha.bu@gmail.com>
2024-12-13 12:30:07 +0700

    updateDeploy

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -9,6 +9,11 @@ pm2 restart keetlo-api
 echo "Deploy script completed on keetlo api"
 
 # CLIENT deploying
-
+echo "Deploy script started on keetlo socket"
+cd /mnt/xvdd/keetlo/socket
+git pull origin socket
+npm install
+pm2 restart keetlo-socket
+echo "Deploy script completed on keetlo socket"
 
 # SOCKET deploying
