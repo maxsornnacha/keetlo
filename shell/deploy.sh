bc9067d9c672297f2a80a25df509d9c9f146f1c2
Sornnacha Buranapongwattana <sornnacha.bu@gmail.com>
2024-12-16 13:51:34 +0700

    update

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -5,7 +5,6 @@ echo "Deploy script started on keetlo api"
 cd /mnt/xvdd/keetlo/api
 git pull origin api
 npm install
-pm2 restart keetlo-api
 echo "Deploy script completed on keetlo api"
 
 # SOCKET deploying
@@ -13,7 +12,11 @@ echo "Deploy script started on keetlo socket"
 cd /mnt/xvdd/keetlo/socket
 git pull origin socket
 npm install
-pm2 restart keetlo-socket
 echo "Deploy script completed on keetlo socket"
 
 # CLIENT deploying
+
+
+# RUN pm2 again
+pm2 restart keetlo-api
+pm2 restart keetlo-socket
